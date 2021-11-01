1193b4709a88dff9f70e5deaf7197e1a31e1e3b1
root <nwindesh@uccs.edu>
2021-11-01 22:45:48 +0000

    Adding rspec code comments

--- spec/example/first_spec.rb
+++ spec/example/first_spec.rb
@@ -2,12 +2,13 @@ require "rails_helper"
 
 RSpec.describe "The math below is wrong..." do
   it "should equal 42 but we said 43" do
+    # Expect an expression to equal some value
     expect(6 * 7).to eq(42)
   end
 end
 
 RSpec.describe "hello spec" do
-  # ...
+  # Creates a string value then checks if it is the empty string
   describe String do
     let(:string) { String.new }
     it "should provide an empty string" do
