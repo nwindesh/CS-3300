796cf119894100366b978e6a35b5b207a54b10ce
root <nwindesh@uccs.edu>
2021-12-04 20:34:30 +0000

    Adding navbar and testing login.

--- Gemfile
+++ Gemfile
@@ -28,6 +28,8 @@ gem 'jbuilder', '~> 2.5'
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
+gem 'bootstrap', '~> 4.6.1'
+gem 'jquery-rails'
 
 group :production do
   gem 'pg', '~> 0.21' # for Heroku deployment
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :controller do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe User, type: :model do
+
+  describe 'user registration' do
+    it "name, email, password and password_You can register if confirmation exists" do
+      user = build(:user)
+      expect(user).to be_valid
+    # user.valid?Pass if is true 
+    end
+  end
+
+
 end
